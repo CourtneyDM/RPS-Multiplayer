abf7d52f01f1f4aba40d13a395c3e6da93f7f74f
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-12 22:28:44 -0500

    Initialize Firebase

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCCfkZ7LRh2HASpKKmYtKqZVSDdmMB8AGw",
+        authDomain: "rps-multiplayer-41636.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-41636.firebaseio.com",
+        projectId: "rps-multiplayer-41636",
+        storageBucket: "",
+        messagingSenderId: "455751164226"
+    };
+    firebase.initializeApp(config);
+});
