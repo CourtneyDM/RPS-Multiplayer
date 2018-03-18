a5a849bcbe1fc6aa1886c9331bdad5c72801a876
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-18 00:29:29 -0500

    final commit for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,39 @@
-$(document).ready(function () {
 
-    /*** Set Global Variables ***/
-    var firstPlayerSelected = false;
-    var playerID = 0;
-    var losses = 0;
-    var wins = 0;
+/*** Set Global Variables ***/
+var losses = 0;
+var wins = 0;
+var playerID = 1;
+var player;
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCCfkZ7LRh2HASpKKmYtKqZVSDdmMB8AGw",
+    authDomain: "rps-multiplayer-41636.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-41636.firebaseio.com",
+    projectId: "rps-multiplayer-41636",
+    storageBucket: "",
+    messagingSenderId: "455751164226"
+};
+firebase.initializeApp(config);
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCCfkZ7LRh2HASpKKmYtKqZVSDdmMB8AGw",
-        authDomain: "rps-multiplayer-41636.firebaseapp.com",
-        databaseURL: "https://rps-multiplayer-41636.firebaseio.com",
-        projectId: "rps-multiplayer-41636",
-        storageBucket: "",
-        messagingSenderId: "455751164226"
+// Reference to database
+var database = firebase.database();
+
+/********************** Begin Checking for Connection State ****************************/
+
+// All players will be stored in this directory
+var connectionsRef = database.ref("players/" + playerID);
+
+// Reference for when a client's connection state changes
+var connectedRef = database.ref(".info/connected");
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+    var name = document.getElementById("player").value.trim();
+    player = {
+        name: name,
+        wins: wins,
+        losses: losses
     };
-    firebase.initializeApp(config);
-
-    // Reference to database
-    var database = firebase.database();
-
-    // All players will be stored in this directory
-    var playersRef = database.ref("players/");
-
-    // Reference for when a client's connection state changes
-    var connectedRef = database.ref(".info/connected");
-
-    /********************** Begin Checking for Connection State ****************************/
-    // When the client's connection state changes...
-    connectedRef.on("value", function (connected) {
-
-        // If they are connected..
-        if (connected.val()) {
-            console.log(connected.val());
-
-            // Add user to the connections list.
-            var con = playersRef.push(true);
-
-            // create a variable and assign it to a function that will add a user to the database
-
-            // Remove user from the connection list when they disconnect.
-            con.onDisconnect().remove();
-        }
-    });
-
-    // When first loaded or when the connections list changes...
-    playersRef.on("value", function (snap) {
-
-        // Display the viewer count in the html.
-        // The number of online users is the number of children in the connections list.
-        $("#player_one").text(snap.numChildren());
-    });
-
-    /********************** Done Checking for Connection State ************************/
-
-
-
-    // document.getElementById("select_player").addEventListener("click", function () {
-
-
-    //     var name = document.getElementById("player_name").value.trim();
-    //     console.log(name);
-    //     var player = [playerID, {
-    //         name: name,
-    //         wins: wins,
-    //         losses: losses
-    //     }];
-    //     // playersRef.push(player);
-    //     database.ref("players/").push(player);
-    // });
 });
-
 
 
 // Things to consider:
